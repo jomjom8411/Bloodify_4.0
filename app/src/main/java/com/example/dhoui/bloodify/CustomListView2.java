780f0b31e69514eca739a35aae039f8fa22d2522
marwen <mahmoud.marwen@esprit.tn>
2018-12-25 16:15:23 +0100

    Yo man I am still alive 4.0

---
+++ app/src/main/java/com/example/dhoui/bloodify/CustomListView2.java
@@ -0,0 +1,446 @@
+package com.example.dhoui.bloodify;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.Display;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+
+import java.io.InputStream;
+import java.util.Locale;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ListView;
+
+import java.io.BufferedInputStream;
+import java.util.ArrayList;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import java.util.Arrays;
+
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.ArrayList;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Calendar;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Calendar;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import android.widget.AdapterView;
+
+
+
+
+/**
+ * Created by jaiso on 13-02-2018.
+ */
+
+public class CustomListView2 extends ArrayAdapter<String>{
+
+    private String[] profilename;
+    private String[] email;
+    private String[] imagepath;
+    BufferedInputStream is;
+    String line = null;
+    String result = null;
+    private Activity context;
+    Bitmap bitmap;
+    private static String URL_CONFIRM = "http://192.168.1.6/blood/confirmdonate.php";
+    String urladdress="http://192.168.1.6/blood/posts_that_a_user_wants_to_donate_on_but_not_confirmed.php?id_user=";
+    String getId;
+    SessionManager sessionManager;
+
+
+
+    public CustomListView2 (Activity context,String[] profilename,String[] email,String[] imagepath) {
+        super(context, R.layout.layout2,profilename);
+        this.context=context;
+        this.profilename=profilename;
+        this.email=email;
+        this.imagepath=imagepath;
+    }
+
+    @NonNull
+    @Override
+
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent){
+
+
+        sessionManager = new SessionManager(getContext());
+        sessionManager.checkLogin();
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        getId = user.get(sessionManager.ID);
+
+
+
+
+        View r=convertView;
+        ViewHolder viewHolder=null;
+        if(r==null){
+            LayoutInflater layoutInflater=context.getLayoutInflater();
+            r=layoutInflater.inflate(R.layout.layout2,null,true);
+            viewHolder=new ViewHolder(r);
+            r.setTag(viewHolder);
+        }
+        else {
+            viewHolder=(ViewHolder)r.getTag();
+
+        }
+
+        viewHolder.tvw1.setText(profilename[position]);
+        viewHolder.tvw2.setText(email[position]);
+        new GetImageFromURL(viewHolder.ivw).execute(imagepath[position]);
+        viewHolder.confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                confirm(position);
+                Toast.makeText(getContext(), "confirm donation" , Toast.LENGTH_SHORT).show();
+
+            }
+        })
+        ;
+
+
+
+
+
+
+
+        return r;
+    }
+
+    class ViewHolder{
+
+        TextView tvw1;
+        TextView tvw2;
+        ImageView ivw;
+        Button confirm;
+
+        ViewHolder(View v){
+            tvw1=(TextView)v.findViewById(R.id.tvprofilename);
+            tvw2=(TextView)v.findViewById(R.id.tvemail);
+            ivw=(ImageView)v.findViewById(R.id.imageView);
+            confirm =v.findViewById(R.id.confirm);
+        }
+
+    }
+
+    public class GetImageFromURL extends AsyncTask<String,Void,Bitmap>
+    {
+
+        ImageView imgView;
+        public GetImageFromURL(ImageView imgv)
+        {
+            this.imgView=imgv;
+        }
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            String urldisplay=url[0];
+            bitmap=null;
+
+            try{
+
+                InputStream ist=new java.net.URL(urldisplay).openStream();
+                bitmap= BitmapFactory.decodeStream(ist);
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap){
+
+            super.onPostExecute(bitmap);
+            imgView.setImageBitmap(bitmap);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void confirm( final int position) {
+
+
+
+
+        try {
+
+            URL url = new URL(urladdress+getId);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            is = new BufferedInputStream(con.getInputStream());
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        //content
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            result = sb.toString();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+
+        }
+
+
+
+
+        StringRequest StringRequest = new StringRequest(Request.Method.POST, URL_CONFIRM, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+
+                    if (success.equals("1")) {
+                        Toast.makeText(getContext(), "donation confirmée!", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "Erreur !" + e.toString(), Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "Erreur !" + error.toString(), Toast.LENGTH_SHORT).show();
+
+                    }
+                }) {
+
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+
+
+
+
+
+                try {
+                    JSONArray ja2 = new JSONArray(result);
+
+                    JSONObject jo2 = null;
+                    //     testpost = new String[ja2.length()];
+                    for(int i=0;i<=ja2.length();i++) {
+
+                        jo2 = ja2.getJSONObject(position);
+                        String e = jo2.getString("id_post");
+
+                        System.out.println("saslut jomjojjmmmmmmm" + position);
+
+                        params.put("id_user", getId);
+                        params.put("id_post", e);
+                        params.put("etat", "1");
+                    }
+                } catch (Exception ex) {
+
+                    ex.printStackTrace();
+                }
+
+
+                return params;
+
+
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(StringRequest);
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/dhoui/bloodify/HistoriqueConfirme.java
@@ -0,0 +1,231 @@
+package com.example.dhoui.bloodify;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+public class HistoriqueConfirme extends AppCompatActivity {
+
+
+    String urladdress2 = "http://192.168.1.6/blood/displayposts.php";
+    String[] name;
+    String[] email,email1;
+    String[] imagepath;
+    ListView listView;
+    BufferedInputStream is;
+    String line=null;
+    String result=null;
+    String result2 = null;
+    String[] test, test2, test3;
+    String getId,getName;
+    SessionManager sessionManager;
+    String urladdress="http://192.168.1.6/blood/posts_that_a_user_wants_to_donate_on_confirmed.php?id_user=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historique_activity);
+
+        sessionManager = new SessionManager(this);
+        HashMap<String, String> user = sessionManager.getUserDetail();
+        getId = user.get(sessionManager.ID);
+        getName= user.get(sessionManager.NAME);
+
+        listView=(ListView)findViewById(R.id.lview);
+
+        StrictMode.setThreadPolicy((new StrictMode.ThreadPolicy.Builder().permitNetwork().build()));
+        collectData();
+        CustomListView3 customListView=new CustomListView3(this,name,email,imagepath);
+        listView.setAdapter(customListView);
+
+    }
+
+
+    private void collectData()
+    {
+//Connection
+        try{
+
+            URL url=new URL(urladdress+getId);
+            HttpURLConnection con=(HttpURLConnection)url.openConnection();
+            con.setRequestMethod("GET");
+            is=new BufferedInputStream(con.getInputStream());
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace
+                    ();
+        }
+        //content
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb=new StringBuilder();
+            while ((line=br.readLine())!=null){
+                sb.append(line+"\n");
+            }
+            is.close();
+            result=sb.toString();
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+
+        }
+
+
+
+        //Connection
+
+
+
+
+
+        try{
+
+
+
+            URL url2=new URL(urladdress2);
+            HttpURLConnection con=(HttpURLConnection)url2.openConnection();
+            con.setRequestMethod("GET");
+            is=new BufferedInputStream(con.getInputStream());
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace
+                    ();
+        }
+        //content
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb=new StringBuilder();
+            while ((line=br.readLine())!=null){
+                sb.append(line+"\n");
+            }
+            is.close();
+            result2=sb.toString();
+
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+
+        }
+
+
+
+
+
+
+
+
+
+//JSON
+
+
+        try{
+            JSONArray ja=new JSONArray(result);
+            JSONObject jo=null;
+            name=new String[ja.length()];
+            email=new String[ja.length()];
+            imagepath=new String[ja.length()];
+            test=new String[ja.length()];
+            test2=new String[ja.length()];
+            for(int i=0;i<=ja.length();i++){
+                jo=ja.getJSONObject(i);
+
+                test[i] = jo.getString("id_user");
+
+                test2[i] = jo.getString("id_post");
+
+
+                if (test[i].equals(getId)) {
+
+
+
+
+
+
+
+
+                    try{
+                        JSONArray ja2=new JSONArray(result2);
+                        JSONObject jo2=null;
+
+
+                        test3=new String[ja2.length()];
+
+                        for(int j=0;j<=ja2.length();j++){
+                            jo2=ja2.getJSONObject(j);
+                            test3[j] = jo2.getString("Id");
+
+                            if (test2[i].equals(test3[j])) {
+
+                                email[i] = "Vous avez Repondu à la demande : " + jo2.getString("slots") + " poches de " + jo2.getString("grpsanguin") + " à " + jo2.getString("region");
+
+
+
+                                name[i] = getName;
+                                //email[i]=jo.getString("id_post");
+                                imagepath[i] = "http://192.168.1.6/blood/local/thanks.jpg";
+                            }
+
+
+
+                        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    }
+
+                    catch (Exception ex)
+                    {
+
+                        ex.printStackTrace();
+                    }
+
+
+                }}
+
+
+
+
+
+        }
+
+
+
+        catch (Exception ex)
+        {
+
+            ex.printStackTrace();
+        }
+
+
+    }
+
+
+
+
+}
